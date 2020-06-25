f0db9f6b3e62c4c97cf5ac13c3de3129f8a8ea56
LinLin--LL <ll3284@columbia.edu>
2020-06-25 12:06:18 -0400

    created flask api endpoint

---
+++ FlaskAPI/requests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 12:02:03 2020
+
+@author: lllin
+"""
+
+import requests 
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+r.json()
